a69273fac7ecebda30775f422ea7897dac649edd
Cody Norwood <CodyBonez@yahoo.com>
2025-11-04 16:00:53 -0800

    Add project files.

---
+++ StructAdapter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StructAdapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
